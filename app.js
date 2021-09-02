5a3144e2f70facd5f8d49d99a5574acd8ac26cfa
ta-moli <yarnusha7@gmail.com>
2021-09-02 14:46:49 +0300

    partials

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const exhbs = require('express-handlebars');
+const products = require('./products.json');
 
 const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'hbs');
+app.engine('hbs', exhbs({
+  extname: 'hbs',
+   })
+);
+
+// http://localhost:4444/
+app.get('/', (req, res) => {
+      res.render('home');
+});
+
+// http://localhost:4444/about
+app.get('/about', (req, res) => {
+   res.render('about', { cssFileName: 'about' });
+});
+
+// http://localhost:4444/products
+app.get('/products', (req, res) => {
+   res.render('products', { products, cssFileName: 'products' });
+});
 
 app.listen(4444, () => {
    console.log('Application server is running on port ${4444}');
